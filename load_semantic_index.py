93806f5612308d294df7f2cd6eb19c168f1fbc7c
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-03-21 09:20:41 -0500

    Load plays one at a time.

--- load_semantic_index.py
+++ load_semantic_index.py
@@ -1,46 +1,59 @@
 from db import db_connect
-from foo import shakespeare_lines
 from task_log import start_task, end_task
-from more_itertools import batched
-
-start_task("Importing sentence_transformers")
-
-from sentence_transformers import SentenceTransformer, util
-
-end_task()
+from lxml import etree
+import sys
 
 
-start_task("Loading model")
-model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-end_task()
+cached_model = None
 
-with db_connect() as conn:
-    with conn.cursor() as cur:
-        cur.execute("CREATE EXTENSION if not exists vector;")
-        cur.execute("drop table if exists chunks;")
-        cur.execute(
-            """
-                create table chunks (
-                    id bigserial primary key,
-                    embedding vector (384),
-                    file_name text,
-                    xpath text,
-                    text text
-                );
-            """
+
+def load_model():
+    global cached_model
+    if cached_model is None:
+        start_task("Importing sentence_transformers")
+
+        from sentence_transformers import SentenceTransformer
+
+        end_task()
+
+        start_task("Loading model")
+        cached_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+        end_task()
+
+    return cached_model
+
+
+def load_play(file_name):
+    start_task("Loading lines")
+    tree = etree.parse("shaks200/" + file_name)
+    lines = [
+        (
+            str(tree.getpath(line)),
+            str(line.text),
         )
+        for line in tree.findall("//SPEECH/LINE")
+    ]
+    end_task()
 
-for i, lines in enumerate(batched(shakespeare_lines(), 500)):
-    start_task(f"Processing batch {i}")
-    sentences = [text for (_, _, text) in lines]
+    model = load_model()
 
-    embeddings = model.encode(sentences)
+    start_task("Computing embeddings")
+    embeddings = model.encode([text for (_, text) in lines])
+    end_task()
 
+    start_task("Writing to database")
     with db_connect() as conn:
-        for (file_name, xpath, text), embedding in zip(lines, embeddings):
-            with conn.cursor() as cur:
+        with conn.cursor() as cur:
+            for (xpath, text), embedding in zip(lines, embeddings):
                 cur.execute(
                     "insert into chunks (embedding, file_name, xpath, text) values (%s, %s, %s, %s);",
                     ([float(e) for e in embedding], file_name, xpath, text),
                 )
     end_task()
+
+
+if __name__ == "__main__":
+    for file_name in sys.argv[1:]:
+        if not file_name.endswith(".xml"):
+            continue
+        load_play(file_name)

256ae08f0f21ef3bd84396e0437df40a26c1de00
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-03-21 08:26:33 -0500

    Make a reset db script.

---
+++ db_reset.py
@@ -0,0 +1,23 @@
+from db import db_connect
+
+
+def db_reset():
+    with db_connect() as conn:
+        with conn.cursor() as cur:
+            cur.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+            cur.execute("DROP TABLE IF EXISTS chunks;")
+            cur.execute(
+                """
+                    CREATE TABLE chunks (
+                        id bigserial PRIMARY KEY,
+                        embedding vector(384),
+                        file_name text,
+                        xpath text,
+                        text text
+                    );
+                """
+            )
+
+
+if __name__ == "__main__":
+    db_reset()

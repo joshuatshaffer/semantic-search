749a5c7d0ebc171f692c4787bb7c459fe1baad8c
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-03-21 09:21:35 -0500

    Use with statement for task logging.

--- load_semantic_index.py
+++ load_semantic_index.py
@@ -1,5 +1,5 @@
 from db import db_connect
-from task_log import start_task, end_task
+from task_log import task
 from lxml import etree
 import sys
 
@@ -10,46 +10,37 @@ cached_model = None
 def load_model():
     global cached_model
     if cached_model is None:
-        start_task("Importing sentence_transformers")
-
-        from sentence_transformers import SentenceTransformer
-
-        end_task()
-
-        start_task("Loading model")
-        cached_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-        end_task()
-
+        with task("Importing sentence_transformers"):
+            from sentence_transformers import SentenceTransformer
+        with task("Loading model"):
+            cached_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
     return cached_model
 
 
 def load_play(file_name):
-    start_task("Loading lines")
-    tree = etree.parse("shaks200/" + file_name)
-    lines = [
-        (
-            str(tree.getpath(line)),
-            str(line.text),
-        )
-        for line in tree.findall("//SPEECH/LINE")
-    ]
-    end_task()
+    with task("Loading lines"):
+        tree = etree.parse("shaks200/" + file_name)
+        lines = [
+            (
+                str(tree.getpath(line)),
+                str(line.text),
+            )
+            for line in tree.findall("//SPEECH/LINE")
+        ]
 
     model = load_model()
 
-    start_task("Computing embeddings")
-    embeddings = model.encode([text for (_, text) in lines])
-    end_task()
+    with task("Computing embeddings"):
+        embeddings = model.encode([text for (_, text) in lines])
 
-    start_task("Writing to database")
-    with db_connect() as conn:
-        with conn.cursor() as cur:
-            for (xpath, text), embedding in zip(lines, embeddings):
-                cur.execute(
-                    "insert into chunks (embedding, file_name, xpath, text) values (%s, %s, %s, %s);",
-                    ([float(e) for e in embedding], file_name, xpath, text),
-                )
-    end_task()
+    with task("Writing to database"):
+        with db_connect() as conn:
+            with conn.cursor() as cur:
+                for (xpath, text), embedding in zip(lines, embeddings):
+                    cur.execute(
+                        "insert into chunks (embedding, file_name, xpath, text) values (%s, %s, %s, %s);",
+                        ([float(e) for e in embedding], file_name, xpath, text),
+                    )
 
 
 if __name__ == "__main__":
--- task_log.py
+++ task_log.py
@@ -1,23 +1,13 @@
 import time
+from contextlib import contextmanager
 
 
-task_start: int | None = None
-
-
-def start_task(task_name: str):
-    global task_start
-    if task_start is not None:
-        print("Oi! You forgot to end the previous task!")
-        return
+@contextmanager
+def task(task_name: str):
     print(task_name, end=" ", flush=True)
     task_start = time.perf_counter_ns()
-
-
-def end_task():
-    global task_start
-    task_end = time.perf_counter_ns()
-    if task_start is None:
-        print("Oi! You forgot to start the task!")
-        return
-    print(f"✓ in {task_end - task_start:,d} nanoseconds")
-    task_start = None
+    try:
+        yield
+    finally:
+        task_end = time.perf_counter_ns()
+        print(f"✓ in {task_end - task_start:,d} nanoseconds")
